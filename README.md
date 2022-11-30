89d9b33f21ab734a5ec3b8707895f1101bbdb5b2
kelvin.silva <kelvin.silva@externo.cresol.coop.br>
2022-11-30 17:35:18 -0300

    PRefactor assando parâmetros no BDD e reaproveitando steps

--- README.md
+++ README.md
@@ -6,3 +6,14 @@ ex passando uma variavel
 mvn test -Dtest=RunCucumber -Dbrowser=chrome cluecumber-report:reporting
 
 ![img_1.png](img_1.png)
+
+
+git push -u origin main
+
+echo "# selenium-java-avancado" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/kelvinsilva1995/selenium-java-avancado.git
+git push -u origin main
---
+++ src/test/java/runner/RunCucumber.java
@@ -0,0 +1,24 @@
+package runner;
+
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import org.junit.AfterClass;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"json:target/reports/cucumberTests.json", "html:target/cucumber/report.html"},
+        features = "src/test/resources/features",
+        tags = "@cadastro_usuario_sucesso",
+        glue = {"steps"}
+
+)
+public class RunCucumber extends RunBase{
+
+    @AfterClass
+    public static void stop(){
+        getDriver().quit();
+    }
+
+}
